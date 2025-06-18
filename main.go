a5c5f31c7ad3de5f9949d0928e6d012cd6232129
pmo <pmo@ZenBook-UX434FL>
2025-06-18 14:09:59 +0200

    the answer

--- main.go
+++ main.go
@@ -69,8 +69,6 @@ func main() {
 	staticFs := http.FileServer(getStaticFiles())
 
 	mux := http.NewServeMux()
-  the_answer := 42
-  fmt.Printf("The answer is %s\n" the_answer)
 
 	// routes
 	mux.HandleFunc("/api", receiveJSONHandler)
